14f70f1aaca7ec095b2735cc9021d9935df19913
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-07-05 14:41:47 +0530

    Create SAA2_Solution.py

---
+++ SAA2_Solution.py
@@ -0,0 +1,59 @@
+import pygame
+pygame.init() 
+
+
+
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption("Breakout Game")
+paddle=pygame.Rect(300,500,60,10)
+ball=pygame.Rect(200,250,10,10)
+ballx=-1
+bally=-1
+paddlex=2
+
+bricksR=[pygame.Rect(10 + i* 100,60,80,30) for i in range(6)]
+bricksO=[pygame.Rect(10 + i* 100,100,80,30) for i in range(6)]
+#Create yellow bricks here
+bricksY=[pygame.Rect(10 + i* 100,140,80,30) for i in range(7)]
+
+carryOn = True
+while carryOn:
+    for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                  carryOn = False # Flag that we are done so we exit this loop             
+    screen.fill((36,90,190))
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+            if paddle.x<540: 
+                paddle.x+=paddlex
+        if event.key == pygame.K_LEFT:
+            if paddle.x>0:
+                paddle.x-=paddlex
+           
+    pygame.draw.rect(screen,(0,176,240),paddle)
+    ball.x=ball.x+ballx
+    ball.y=ball.y+bally
+    if ball.x>=590:
+        ballx=-ballx
+    if ball.x<=10:
+        ballx=-ballx
+    if ball.y>=590:
+        bally=-bally
+    if ball.y<=10:
+        bally=-bally
+    if paddle.collidepoint(ball.x,ball.y):
+        bally=-bally
+ 
+    for i in bricksR:
+        pygame.draw.rect(screen,RED,i)
+    for i in bricksO:
+        pygame.draw.rect(screen,ORANGE,i)
+     #Draw the yellow bricks here.
+     for i in bricksY:
+        pygame.draw.rect(screen,YELLOW,i)
+    
+   
+    pygame.draw.rect(screen,(255,255,255) ,ball)
+    pygame.display.flip()
+pygame.quit()
+    
